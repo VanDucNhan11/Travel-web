dbd012cea0cefebeff4b75e33d8c22b38cb4f653
VanDucNhan11 <vanducnhan1102@gmail.com>
2024-09-12 23:11:19 +0700

    navBar and home done ui mobile

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './navbar.css'
 import {MdOutlineTravelExplore} from 'react-icons/md'
 import {IoIosCloseCircle} from 'react-icons/io'
 import {TbGridDots} from 'react-icons/tb'
 
 const Navbar = () => {
+    const [active, setActive] = useState('navBar')
+    const showNav = () =>{
+        setActive('navBar activeNavBar')
+    }
+    const removeNav = () =>{
+        setActive('navBar')
+    }
   return (
     <section className="navBarSection">
         <header className="header flex">
@@ -13,7 +20,7 @@ const Navbar = () => {
                     <h1 ><MdOutlineTravelExplore className="icon"/> Travel.</h1>
                 </a>  
             </div>
-            <div className="navBar">
+            <div className={active}>
                 <ul className="navLists flex">
                     <li className="navItem">
                         <a href="#" className="navLink">Home</a>
@@ -31,13 +38,13 @@ const Navbar = () => {
                         <a href="#">Book Now</a>
                     </button>
                 </ul>
-                <div className="closeNavBar">
+                <div onClick={removeNav} className="closeNavBar">
                     <IoIosCloseCircle className="icon"/>
                 </div>
 
                 
             </div>
-            <div className="toggleNavbar">
+            <div onClick={showNav} className="toggleNavbar">
                 <TbGridDots className="icon" />
             </div>
         </header>
